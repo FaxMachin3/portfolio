4f563f1870f16373770e9d11f20116ca0bc1c359
Subham Raj <subhamraj4114@gmail.com>
2020-01-31 19:35:01 +0530

    Updates

---
+++ precache-manifest.b5f803e031be9440e0820b4e600f1aa8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9116c76948416c22470703b12e6ff881",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "10cb14959d39d0ae54c3",
+    "url": "/portfolio/static/css/main.a67ee658.chunk.css"
+  },
+  {
+    "revision": "ab2b2c313df276ad3443",
+    "url": "/portfolio/static/js/2.e925faae.chunk.js"
+  },
+  {
+    "revision": "10cb14959d39d0ae54c3",
+    "url": "/portfolio/static/js/main.abc2d55e.chunk.js"
+  },
+  {
+    "revision": "4234e20402381065f478",
+    "url": "/portfolio/static/js/runtime-main.01d0503f.js"
+  },
+  {
+    "revision": "6eb12d19569988060363698fd48b577e",
+    "url": "/portfolio/static/media/APS.6eb12d19.jpg"
+  },
+  {
+    "revision": "7644de82e804c8108036b2764476f815",
+    "url": "/portfolio/static/media/avatar.7644de82.jpg"
+  },
+  {
+    "revision": "9011bc0ac1ed699556e0c802b95f0816",
+    "url": "/portfolio/static/media/bookMyEvent.9011bc0a.jpg"
+  },
+  {
+    "revision": "82211aca38ea347538c2ef945ddd9250",
+    "url": "/portfolio/static/media/darkWeather.82211aca.jpg"
+  },
+  {
+    "revision": "4c32a30b387629a59cbde85793de141d",
+    "url": "/portfolio/static/media/eCommerce.4c32a30b.jpg"
+  },
+  {
+    "revision": "f54edd1f874e0a5d291eece5002671b6",
+    "url": "/portfolio/static/media/myPortfolio.f54edd1f.jpg"
+  }
+]);
